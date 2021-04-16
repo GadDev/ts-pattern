a8d89beaa67c00cb77b1159ecac951643f6e89d6
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-16 09:10:54 +0100

    pattern folder

---
+++ patterns/decorator.ts
@@ -0,0 +1,17 @@
+class ComputerA {
+	public description = 'Computer A';
+	public price = 2000;
+
+	public cost(): number {
+		return this.price;
+	}
+}
+
+class ComputerB {
+	public description = 'Computer B';
+	public price = 3000;
+
+	public cost(): number {
+		return this.price;
+	}
+}
